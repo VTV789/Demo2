0632494cd2d21fe047eb97cf465092625dc48be2
Vinh Thanh Vu <vtv@vinh.com>
2016-01-07 22:17:17 -0500

    First Commit

---
+++ Demo2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Demo2
+//
+//  Created by Vinh Vu on 1/7/16.
+//  Copyright © 2016 Vinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
