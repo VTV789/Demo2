6218f0dbd069d1abaee709e27c3d8c01eda35b25
Vinh Thanh Vu <vtv@vinh.com>
2016-01-07 22:39:10 -0500

    Presenting and closing the alert view

--- Demo2/ViewController.swift
+++ Demo2/ViewController.swift
@@ -12,9 +12,20 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
     }
-
-
+    
+    @IBAction func experiment(sender: AnyObject) {
+    
+        let controller = UIAlertController()
+        controller.title = "Test alert"
+        controller.message = "This is a test"
+        
+        // Dismiss the view controller after the user taps “ok”
+        let okAction = UIAlertAction (title:"ok", style: UIAlertActionStyle.Default) {
+            action in self.dismissViewControllerAnimated(true, completion: nil)
+        }
+        controller.addAction(okAction)
+        self.presentViewController(controller, animated: true, completion:nil)
+    }
 }
 
